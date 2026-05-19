295f889ad03b9ea0e278bcb339d384032efe4d99
softwaredev <softwaredev509@gmail.com>
2026-05-19 13:29:39 -0400

    add .env.example

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import json
 import os
 import websockets
 import httpx
+from dotenv import load_dotenv
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+
+load_dotenv()
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
